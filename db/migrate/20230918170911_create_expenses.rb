045672b775e78876f675bfe4d0bffc4005408943
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-27 15:07:34 -0300

    Added migrations

--- db/migrate/20230918170911_create_expenses.rb
+++ db/migrate/20230918170911_create_expenses.rb
@@ -1,7 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :name
       t.decimal :amount
 
