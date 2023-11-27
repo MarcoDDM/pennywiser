5a846e9f6af530611b38f11dabcd2ba18bb801da
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-27 15:20:25 -0300

    Update controllers and schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :load_and_authorize_resource
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:index], unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
+  protected
+
+  def after_sign_in_path_for(_resource)
+    categories_path
   end
 
-  private
-
-  def load_and_authorize_resource
-    @current_ability ||= Ability.new(current_user)
-    @current_ability.authorize!(params[:action].to_sym, resource)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
