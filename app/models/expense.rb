4b628053910bfdf09c6a5be5fc52af56ff59d5b6
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-27 14:27:48 -0300

    Add validations to models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,7 @@
 class Expense < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  belongs_to :group, optional: true
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
