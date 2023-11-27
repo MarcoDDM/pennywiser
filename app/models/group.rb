f33875132c0530ae2bf5a46b24ccba9616ff421b
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-27 14:27:48 -0300

    Add validations to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  has_many :expenses
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
+class User < ApplicationRecord
+  validates :name, presence: true
 end
