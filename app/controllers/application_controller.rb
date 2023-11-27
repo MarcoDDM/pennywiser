1c48ad634e5acd34e4af8519293cdc29ae15228e
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-27 14:41:50 -0300

    Add authorization and validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :load_and_authorize_resource
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
+  private
+
+  def load_and_authorize_resource
+    @current_ability ||= Ability.new(current_user)
+    @current_ability.authorize!(params[:action].to_sym, resource)
+  end
 end
