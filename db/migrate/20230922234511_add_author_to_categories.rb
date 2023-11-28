8cb60a05b779527f46b250cede69b24549f3316d
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-28 13:49:02 -0300

    Update migrations

--- db/migrate/20230922234511_add_author_to_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToCategories < ActiveRecord::Migration[7.0]
-  def change
-    add_column :categories, :author_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_191116) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_004945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_191116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.integer "author_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
